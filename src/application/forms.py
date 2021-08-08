1e4d842ae76049eb8b49197de580efb6d15308e8
Dara Oladapo <daraoladapo@outlook.com>
2021-08-08 17:26:18 +0100

    rooutes and models

--- src/application/forms.py
+++ src/application/forms.py
@@ -14,4 +14,9 @@ class AddStudentForm(FlaskForm):
         students = Students.query.all()
         for student in students:
             if student.first_name == first_name.data and student.last_name==last_name.data and student.email_address==email_address:
-                raise ValidationError('You already added this student')
+                raise ValidationError('You already added this student.')
+class UpdateStudentForm(FlaskForm):
+    first_name = StringField('First Name', validators = [DataRequired()])
+    last_name=StringField('Last Name', validators = [DataRequired()])
+    email_address=StringField('Email Address', validators = [DataRequired()])
+    submit = SubmitField('Submit')
--- src/application/routes.py
+++ src/application/routes.py
@@ -1,14 +1,9 @@
 from flask import render_template, url_for, redirect, request
 from application import app, db
 from application.models import Students
-from application.forms import AddStudentForm
+from application.forms import AddStudentForm, UpdateStudentForm
 
-
-@app.route('/')
-def index():
-    students = Students.query.all()
-    return render_template('index.html', title="Students", students=students)
-
+#create
 @app.route('/add', methods=['POST', 'GET'])
 def add():
     form = AddStudentForm()
@@ -18,10 +13,43 @@ def add():
             last_name = form.last_name.data,
             email_address = form.email_address.data
         )
-        print(student.first_name)
-        print(student.last_name)
-        print(student.email_address)
         db.session.add(student)
         db.session.commit()
         return redirect(url_for('index'))
     return render_template('add.html', title="Add new student", form=form)
+
+#read   
+@app.route('/')
+def index():
+    students = Students.query.all()
+    return render_template('index.html', title="Students", students=students)
+
+@app.route('/details/<int:id>')
+def details(id):
+    student = Students.query.get(id)
+    return render_template('details.html', title=student.first_name, student=student)
+
+#update    
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    form = UpdateStudentForm()
+    student = Students.query.get(id)
+    if form.validate_on_submit():
+        student.first_name = form.first_name.data
+        student.last_name = form.last_name.data
+        student.email_address = form.email_address.data
+        db.session.commit()
+        redirect(url_for('index'))
+    elif request.method == 'GET':
+        form.first_name.data = student.first_name
+        form.last_name.data = student.last_name
+        form.email_address.data = student.email_address
+    return render_template('update.html', title='Update student details', form=form)
+
+#delete
+@app.route('/delete/<int:id>')
+def delete(id):
+    student = Students.query.get(id)
+    db.session.delete(student)
+    db.session.commit()
+    return redirect(url_for('index'))
