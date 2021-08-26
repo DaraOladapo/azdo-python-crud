a6d3fb7d2bc4c23e8205b792341fde6a4582b768
Dara Oladapo <doladapo@qa.com>
2021-08-26 10:43:11 +0100

    removed uncessary files

--- src/initscript.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-sudo apt install python3-pip -y
-# sudo apt install python3-venv -y
-# sudo pip3 install virtualenv
-# python3 -m venv venv
-# source venv/bin/activate
--- src/run.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-sudo apt install gunicorn3
-gunicorn3 --bind=0.0.0.0:5000 app:app
